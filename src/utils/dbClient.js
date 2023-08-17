6f027aa7b07906efa2a1b03619d219fb291bf671
Fazeel <fazeel642@gmail.com>
2023-08-17 12:24:07 +0530

    Rename client to runQuery

--- src/utils/dbClient.js
+++ src/utils/dbClient.js
@@ -3,7 +3,7 @@ const { DEFAULT_ERR_MSG } = require('./constants');
 const { AppError } = require('./errorHandler');
 const { logger } = require('./logger');
 
-module.exports.client = async (query, params) => {
+module.exports.runQuery = async (query, params) => {
     const client = await pool.connect();
     try {
         const result = await client.query(query, params);
