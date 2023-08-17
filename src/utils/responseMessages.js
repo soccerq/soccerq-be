faee3b8799e50f911a1267ad1ba2e12ed3ccb0d8
Fazeel <fazeel642@gmail.com>
2023-08-17 12:24:34 +0530

    Add default response messages as constants

---
+++ src/utils/responseMessages.js
@@ -0,0 +1,11 @@
+module.exports.INTERNAL_SERVER_ERROR_MSG = 'Internal Server Error';
+module.exports.DEFAULT_ERR_MSG = 'An Error occured: ';
+module.exports.UNEXPECTED_ERR_MSG = 'Unexpected Error';
+module.exports.INVALID_USERNAME_PASSWORD_ERR_MSG = 'Invalid username/password';
+module.exports.CREATED_MESSAGE = 'Created successfully';
+module.exports.UPDATED_MESSAGE = 'Updated successfully';
+
+module.exports.MISSING_REQ_BODY = 'Request body is missing';
+module.exports.INVALID_REQ_BODY = 'Request body is invalid';
+module.exports.MISSING_REQ_PARAM = 'Request parameter is missing';
+module.exports.INVALID_REQ_PARAM = 'Request parameter is invalid';
